28783577188d4b6fd37bb2a4beaff27ada061c84
Abhinav-engg <abhinav.pal6.7.9@gmail.com>
2026-09-08 16:21:48 +0530

    chore: initial folder structure

---
+++ js/inputs/inputs.js
@@ -0,0 +1,32 @@
+export function setupInputs(){
+
+
+
+    document.addEventListener('keydown',function(event){
+        const key = event.key.toLowerCase();
+        if(key == 'arrowup' || key == 'w'){
+            
+
+        }
+
+        if(key == 'arrowdown' || key == 's'){
+
+
+        }
+
+        if(key == 'arrowleft' || key == 'a'){
+
+        }
+
+        if(key == 'arrowright' || key == 'd'){
+
+        }
+
+        if(key == ' ' || key == 'spacebar'){
+            //nitro
+        }
+
+        // add for wheelie later(w+Lshift)
+
+    });
+}
---
+++ js/variables/state.js
@@ -0,0 +1,10 @@
+let road = [];
+let tracklength = 0;
+
+let cameraX = 0;
+let cameraZ =0;
+ 
+let playerZ = 0;
+let playerX = 0;
+
+let speed = 0;
