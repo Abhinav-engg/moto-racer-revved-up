a8ccc44236afb846c7197d3f450fa4195d0f1a49
Abhinav-engg <abhinav.pal6.7.9@gmail.com>
2026-09-08 21:27:50 +0530

    feat: add keyboard input state

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+function update() {
+
+}
+
+function draw() {
+    
+    
+    
+
+
+
+}
+
+function gameLoop() {
+    update();
+    draw();
+
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
